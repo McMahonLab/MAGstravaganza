8984a207d15842e7069fce07d99f4c8836a6e1b1
alexlinz <amlinz@wisc.edu>
2017-12-09 18:56:03 -0600

    Started new heatmaps for Fig2 - need to fill in some pathways from IMG

--- Fig2_carbonchart.R
+++ Fig2_carbonchart.R
@@ -1,20 +1,62 @@
 # Make figure from carbon chart
 
-table <- read.csv("C:/Users/Alex/Desktop/MAGstravaganza/Data_files/carbon_chart.csv", header = T)
+table <- read.csv("C:/Users/Alex/Desktop/MAGstravaganza/Pathway_analysis/consolidated_pathway_data.csv", header = T)
+metadata <- read.csv("C:/Users/Alex/Desktop/MAGstravaganza/Supplemental/MAG_information.csv")
 library(ggplot2)
 library(cowplot)
+library(reshape2)
 
 #make a long format table - for each phylum and lake, count how many times each pathway appears.
 
-table$phylum <- sapply(strsplit(as.character(table$Taxonomy.README.),";"), `[`, 1)
-table$class<- sapply(strsplit(as.character(table$Taxonomy.README.),";"), `[`, 2)
-table$order <- sapply(strsplit(as.character(table$Taxonomy.README.),";"), `[`, 3)
+phylum <- sapply(strsplit(as.character(metadata$Taxonomy),";"), `[`, 1)
+class<- sapply(strsplit(as.character(metadata$Taxonomy),";"), `[`, 2)
+order <- sapply(strsplit(as.character(metadata$Taxonomy),";"), `[`, 3)
 
-ME.total <- colSums(table[which(table$Lake == "ME"), 5:29])/102 
-TE.total <- colSums(table[which(table$Lake == "TE"), 5:29])/36
-TH.total <- colSums(table[which(table$Lake == "TH"), 5:29])/64
+colnames(table) <- gsub("X", "", colnames(table))
+table <- table[1:61,]
+for(i in 3:dim(table)[2]){
+  column <- table[,i]
+  column[which(is.na(column) == T)] <- 0
+  table[,i] <- column
+}
+rTCA <- table[2,3:196]
+rTCA[which(rTCA < 100)] <- 0
+table[2,3:196] <- rTCA
+
+table$Pathway <- gsub("(*)", "", table$Pathway)
+ME.MAGs <- table[,which(is.na(match(colnames(table), metadata$IMG_OID[which(metadata$Lake == "Mendota")])) == F)]
+ME.MAGs$Pathways <- make.unique(table$Pathway)
+
+ME.long <- melt(ME.MAGs)
+ME.long$Pathways <- factor(ME.long$Pathways, levels = table$Pathway)
+ME.long$variable <- factor(ME.long$variable, levels = colnames(table))
+ME.long$Taxonomy <- phylum[match(ME.long$variable, metadata$IMG_OID)]
 
 
+ggplot(data = ME.long, aes(y = Pathways, x = Taxonomy, fill = value)) + geom_tile(color = "black") + labs(x = "", y = "") + scale_fill_gradient2(low = "white", high = "#5ab4ac", midpoint = 25) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+TE.MAGs <- table[,which(is.na(match(colnames(table), metadata$IMG_OID[which(metadata$Lake == "Trout Bog Epilimnion")])) == F)]
+TE.MAGs$Pathways <- make.unique(table$Pathway)
+
+TE.long <- melt(TE.MAGs)
+TE.long$Pathways <- factor(TE.long$Pathways, levels = table$Pathway)
+TE.long$variable <- factor(TE.long$variable, levels = colnames(table))
+TE.long$Taxonomy <- phylum[match(TE.long$variable, metadata$IMG_OID)]
+
+
+ggplot(data = TE.long, aes(y = Pathways, x = Taxonomy, fill = value)) + geom_tile(color = "black") + labs(x = "", y = "") + scale_fill_gradient2(low = "white", high = "#67a9cf", midpoint = 25) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+TH.MAGs <- table[,which(is.na(match(colnames(table), metadata$IMG_OID[which(metadata$Lake == "Trout Bog Hypolimnion")])) == F)]
+TH.MAGs$Pathways <- make.unique(table$Pathway)
+
+TH.long <- melt(TH.MAGs)
+TH.long$Pathways <- factor(TH.long$Pathways, levels = table$Pathway)
+TH.long$variable <- factor(TH.long$variable, levels = colnames(table))
+TH.long$Taxonomy <- phylum[match(TH.long$variable, metadata$IMG_OID)]
+
+
+ggplot(data = TH.long, aes(y = Pathways, x = Taxonomy, fill = value)) + geom_tile(color = "black") + labs(x = "", y = "") + scale_fill_gradient2(low = "white", high = "#d8b365", midpoint = 25) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+##############
 lake <- c(rep("ME", 26), rep("TB", 52))
 label <- c(rep("Mendota", 26), rep("TroutEpi", 26), rep("TroutHypo", 26))
 carbon <- rep(colnames(table)[4:29], 3)
