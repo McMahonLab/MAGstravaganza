5200715a3842a29a2dd349351e6f3f8e22781dd0
alexlinz <amlinz@wisc.edu>
2018-01-31 17:52:17 -0600

    Made sulfur edits on manuscript

--- Manuscript_plots/MAGstravanganza_manuscript_plots_2018-01-23.R
+++ Manuscript_plots/MAGstravanganza_manuscript_plots_2018-01-23.R
@@ -9,9 +9,11 @@ library(cowplot)
 library(reshape2)
 library(ape)
 library(zoo)
-
+dadjoke::groan()
 ############
 ### Fig 1. 16S vs metagenomic read classifications
+
+
 
 ############
 ### Fig 2. Metabolism chart
